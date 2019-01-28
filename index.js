91f793e2c417e0ee1a7520b8ecff00d9febbd923
zouyun <thunderrun2035@gmail.com>
2019-01-28 14:26:29 +0800

    add game mode

--- index.js
+++ index.js
@@ -7,6 +7,8 @@ const satelliteLayer = new AMap.TileLayer.Satellite({
 });
 map.add(satelliteLayer);
 
+let gameMode = false;
+
 // set up wasd control
 
 let intervals = { w: null, a: null, s: null, d: null };
@@ -32,6 +34,15 @@ const keyHandler = e => {
 document.onkeydown = keyHandler;
 
 document.onkeyup = e => {
-  clearInterval(intervals[e.key]);
-  document.onkeydown = keyHandler;
+  if (e.key === "g") { // game mode
+    gameMode = !gameMode;
+    if (gameMode) {
+      map.setMapStyle("amap://styles/aebe189d2072666f6fccc6c2d4946af7");
+    } else {
+      map.setMapStyle("amap://styles/ab8e6d6ef2ba3500d70346b36f66dba2");
+    }
+  } else {
+    clearInterval(intervals[e.key]);
+    document.onkeydown = keyHandler;
+  }
 };
