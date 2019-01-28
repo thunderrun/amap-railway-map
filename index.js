8ec05b4802fdf6989c67c82468f4c6a48b025a77
zouyun <thunderrun2035@gmail.com>
2019-01-28 14:18:05 +0800

    update mapStyle

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 // create map
 const map = new AMap.Map("container", {
-  mapStyle: "amap://styles/aebe189d2072666f6fccc6c2d4946af7"
+  mapStyle: "amap://styles/ab8e6d6ef2ba3500d70346b36f66dba2"
 });
 const satelliteLayer = new AMap.TileLayer.Satellite({
   zIndex: -1
